5540d691baf06ab2602826d6a66dc1e716e5e40d
Milosz Galazka <milosz@sleeplessbeastie.eu>
2013-08-31 10:43:22 +0000

    Added notification area. Fixed token problem.
    More info:
    https://mail.kde.org/pipermail/owncloud/2013-May/009073.html
    https://github.com/owncloud/core/commit/f2911e76bcbb88e1da5caa2ee4cebd2d78725999
    https://github.com/owncloud/core/commit/9a53d50e169a797f95c16cb75aa1bcafd7513c76

--- copy_to_owncload/themes/kolab/core/templates/layout.user.php
+++ copy_to_owncload/themes/kolab/core/templates/layout.user.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-	<head>
+	<head data-requesttoken="<?php p($_['requesttoken']); ?>"> 
 		<title><?php echo isset($_['application']) && !empty($_['application'])?$_['application'].' | ':'' ?>ownCloud <?php echo OC_User::getUser()?' ('.OC_User::getUser().') ':'' ?></title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="shortcut icon" href="<?php echo image_path('', 'favicon.png'); ?>" /><link rel="apple-touch-icon-precomposed" href="<?php echo image_path('', 'favicon-touch.png'); ?>" />
@@ -17,7 +17,6 @@
 		<?php foreach($_['jsfiles'] as $jsfile): ?>
 			<script type="text/javascript" src="<?php echo $jsfile; ?>"></script>
 		<?php endforeach; ?>
-		<script type="text/javascript" src="<?php echo OC::$WEBROOT.'/themes/kolab/core/js/kolab.js'; ?>"></script>
 		<?php foreach($_['headers'] as $header): ?>
 			<?php
 				echo '<'.$header['tag'].' ';
@@ -30,6 +29,9 @@
 	</head>
 
 	<body id="<?php echo $_['bodyid'];?>">
+		<div id="notification-container">
+			<div id="notification"></div>
+		</div>
 		<div id="content">
 			<?php echo $_['content']; ?>
 		</div>
